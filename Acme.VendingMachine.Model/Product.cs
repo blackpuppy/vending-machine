9efea7c0adbd23087624312db3b8e896cf89d293
Zhu Ming <mingzhu.z+gitlab@gmail.com>
2019-03-28 00:44:21 +0800

    add model project

---
+++ Acme.VendingMachine.Model/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Acme.VendingMachine.Model
+{
+    public class Product
+    {
+        public int ItemNo { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+    }
+}
