291d7db8c7b91daec7d6bc1be06f35300ca7b4e4
Zhu Ming <mingzhu.z+gitlab@gmail.com>
2019-03-30 21:19:58 +0800

    refactor Product Catalog as View Component

---
+++ Acme.VendingMachine.Web/ViewComponents/ProductCatalogViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Acme.VendingMachine.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.VendingMachine.Web.ViewComponents
+{
+    public class ProductCatalogViewComponent : ViewComponent
+    {
+        private readonly IProductBll _productBll;
+
+        public ProductCatalogViewComponent(IProductBll productBll)
+        {
+            _productBll = productBll;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = await Task.Run(() => _productBll.GetAllProducts());
+            return View(products);
+        }
+    }
+}
